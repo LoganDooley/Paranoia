c516c98127da9329a92d0704c7911e8726f9175c
LoganDooley <logan_dooley@brown.edu>
2021-01-23 18:08:59 -0500

    Added opening scene with start button and created a title theme

---
+++ After School/Assets/Scripts/MoveBus.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveBus : MonoBehaviour
+{
+    public float busSpeed = 3f;
+    public Rigidbody2D rb;
+    private bool active;
+    public AudioSource theme;
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = false;
+    }
+
+    public void start()
+    {
+        active = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (active)
+        {
+            rb.MovePosition(rb.position + Vector2.right * busSpeed * Time.fixedDeltaTime);
+
+            if (rb.position.x >= 15f)
+            {
+                SceneManager.LoadScene("House");
+            }
+            theme.volume = theme.volume * 0.996f;
+        }
+    }
+}
